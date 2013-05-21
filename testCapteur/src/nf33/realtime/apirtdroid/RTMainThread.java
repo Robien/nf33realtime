7a763763b252ffdfc2463ed6da1f41d91f03e68e
cephise.louison@gmail.com <cephise.louison@gmail.com@51f08e5e-7caa-eba2-bc85-e3b261c825e3>
2013-05-21 15:52:37 +0000

    Ajout d'une classe Thread pour gerer l'action principale -> quand on lance le run() dans RTDroid

---
+++ testCapteur/src/nf33/realtime/apirtdroid/RTMainThread.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package nf33.realtime.apirtdroid;
+
+/**
+ * @author Seg_fault_
+ *
+ */
+public class RTMainThread extends Thread
+{
+
+	public void run() {
+	    /*A faire dans la fonction run de RTDroid
+	     * RTMainThread t = new RTMainThread();
+ 
+		  t.livre = hamlet;
+		  t.display = v;
+		 
+		  t.start();
+		  */
+	  }
+	
+}
