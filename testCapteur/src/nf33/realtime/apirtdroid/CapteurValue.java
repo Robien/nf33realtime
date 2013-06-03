253e2a8d38742570a18d01f310d79e62d7e84f7e
cephise.louison@gmail.com <cephise.louison@gmail.com@51f08e5e-7caa-eba2-bc85-e3b261c825e3>
2013-06-03 15:30:25 +0000

    Ajout de la classe de valeur de capteur

---
+++ testCapteur/src/nf33/realtime/apirtdroid/CapteurValue.java
@@ -0,0 +1,50 @@
+package nf33.realtime.apirtdroid;
+
+
+public class CapteurValue
+{
+	long _timestampCaptureAnd; //capture de la valeur par android
+	long _timestampCaptureApi;		//capture de la valeur par l'api
+	float[] _values;
+	private int _id;
+	public CapteurValue(int id, long timestampCaptureAnd, float[] values)
+	{
+		super();
+		this._id = id;
+		this._timestampCaptureAnd = timestampCaptureAnd;
+		this._values = values;
+	}
+	
+	public long getTimestampCaptureAnd()
+	{
+		return _timestampCaptureAnd;
+	}
+	
+	public void setTimestampCaptureAnd(long timestampCaptureAnd)
+	{
+		this._timestampCaptureAnd = timestampCaptureAnd;
+	}
+	
+	public long getTimestampCaptureApi()
+	{
+		return _timestampCaptureApi;
+	}
+	
+	public void setTimestampCaptureApi(long timestampCaptureApi)
+	{
+		this._timestampCaptureApi = timestampCaptureApi;
+	}
+	
+	public float[] getValues()
+	{
+		return _values;
+	}
+	
+	public void setValues(float[] values)
+	{
+		this._values = values;
+	}
+	
+	
+	
+}
