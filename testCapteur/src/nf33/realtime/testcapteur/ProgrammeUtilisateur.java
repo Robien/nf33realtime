64109f510fa059166035d3e545263299f3a15119
r.guyard@gmail.com <r.guyard@gmail.com@51f08e5e-7caa-eba2-bc85-e3b261c825e3>
2013-06-10 16:02:33 +0000

    remise du code moche et copié collé de Céphise

--- testCapteur/src/nf33/realtime/testcapteur/ProgrammeUtilisateur.java
+++ testCapteur/src/nf33/realtime/testcapteur/ProgrammeUtilisateur.java
@@ -16,20 +16,16 @@ public class ProgrammeUtilisateur implements RTRunnable
 	private Handler mHandler;
 	private RTDroid rtdroid;
 	
-	public static final int MESSAGE_RECORD = 2;
-	public static final int MESSAGE_PERIODE = 1;
-	public static final int MESSAGE_FINCONFIG = 0;
-	
 	//vitesse minimun pour détecter un shake
-	private static final int FORCE_THRESHOLD = 20;//350;
+	private static final int FORCE_THRESHOLD = 350;
 	// temps minimun pour commencer a détecter la position du téléphone
-	private static final long TIME_THRESHOLD =100000000l;
+	private static final int TIME_THRESHOLD = 100;
 	// temps minimun entre 2 position pour être considérer comme un shaker
-	private static final long SHAKE_TIMEOUT = 5000000000l;
+	private static final int SHAKE_TIMEOUT = 500;
 	// temps entre un 2 shake pour incrementer le nombre de shake
-	private static final long SHAKE_DURATION = 1000000000l;
+	private static final int SHAKE_DURATION = 1000;
 	// nombre de shake pour lancer une action
-	private static final int SHAKE_COUNT = 1;
+	private static final int SHAKE_COUNT = 3;
 	
 	private SensorManager mSensorMgr;
 	private float mLastX=-1.0f, mLastY=-1.0f, mLastZ=-1.0f;
@@ -52,11 +48,7 @@ public class ProgrammeUtilisateur implements RTRunnable
 	@Override
 	public void endConfiguration(Boolean isRunable, Long frequence, Long wcet)
 	{
-
-		Message msg = mHandler.obtainMessage();
-		msg.obj = new String("Configuration terminée\nfrequence : "+frequence+ "\nwcet : "+ wcet);
-		msg.arg2 = MESSAGE_FINCONFIG;
-        mHandler.sendMessage(msg);
+		// TODO Auto-generated method stub
 		
 	}
 
@@ -101,8 +93,7 @@ public class ProgrammeUtilisateur implements RTRunnable
 		
 		/***************Programme utilisateur***********************/
 		Message msg = mHandler.obtainMessage();
-		msg.arg2 = MESSAGE_PERIODE;
-		msg.obj = new String("Période : ");
+		msg.obj = new String("période : ");
 		msg.arg1 = (int)(timeSinceLast);
         mHandler.sendMessage(msg);
 		if (capteursValues != null)
@@ -112,13 +103,50 @@ public class ProgrammeUtilisateur implements RTRunnable
 				//si le l'un des capteur est un accelerometre
 				if (capteursValues.get(i).getType() == 1)
 				{
-					msg = mHandler.obtainMessage();
-					msg.obj = new String("!! Grosse Secousse !!\n");
-					msg.arg1 = (int)(capteursValues.get(i).getValues()[0]*1000);
-					msg.arg2 = MESSAGE_RECORD;
-		            mHandler.sendMessage(msg);
-					
-					
+					// le temps date de la capture
+					long now = capteursValues.get(i).getTimestampCaptureApi();
+
+
+					// si ce temps et le temps de la dernière position est trop
+					// long, supérieur à SHAKE_TIMEOUT on recommence avec mShakeCount = 0;
+					if ((now - mLastForce) > SHAKE_TIMEOUT)
+					{
+						mShakeCount = 0;
+					}
+
+					//difference entre la dernier calcul et maintenant
+					long diff = now - mLastTime;
+					if (diff > TIME_THRESHOLD)
+					{
+						
+						// calcule de la vitesse de shake
+						float speed = Math.abs(capteursValues.get(i).getValues()[SensorManager.DATA_X] 
+								+ capteursValues.get(i).getValues()[SensorManager.DATA_Y] 
+								+ capteursValues.get(i).getValues()[SensorManager.DATA_Z] - mLastX - mLastY - mLastZ) / diff * 10000;
+						// on passe cette étape si notre vitesse est supérieure à notre vitesse minimum FORCE_THRESHOLD
+						if (speed > FORCE_THRESHOLD)
+						{
+							// si nous avons un nombre de shake supérieur à notre nombre minimun SHAKE_COUNT
+							if ((++mShakeCount >= SHAKE_COUNT) && (now - mLastShake > SHAKE_DURATION))
+							{
+								
+								// si toutes les conditions sont réunies je previens l'interface
+								msg = mHandler.obtainMessage();
+								msg.obj = new String("!! Grosse Secousse !!\n");
+								msg.arg1 = (int)(speed*1000);
+					            mHandler.sendMessage(msg);
+					               
+								mLastShake = now;
+								mShakeCount = 0;			
+							}
+							mLastForce = now;
+						}
+						mLastTime = now;
+						mLastX = capteursValues.get(i).getValues()[SensorManager.DATA_X];
+						mLastY = capteursValues.get(i).getValues()[SensorManager.DATA_Y];
+						mLastZ = capteursValues.get(i).getValues()[SensorManager.DATA_Z];
+
+					}
 				}
 			}
 
